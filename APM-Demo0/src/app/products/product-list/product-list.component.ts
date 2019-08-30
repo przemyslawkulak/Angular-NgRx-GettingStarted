9cbb6d5b3da14709bb1da36ecc05abfcdf00220d
Przemysław Kulak <przemyslaw.kulak86@gmail.com>
2019-08-30 15:52:58 +0200

    selectors and strongly typing

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from "rxjs";
 import { Product } from "../product";
 import { ProductService } from "../product.service";
 import { Store, select } from "@ngrx/store";
+import * as fromProduct from "../state/product.reducer";
 
 @Component({
   selector: "pm-product-list",
@@ -25,7 +26,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   constructor(
     private productService: ProductService,
-    private store: Store<any>
+    private store: Store<fromProduct.State>
   ) {}
 
   ngOnInit(): void {
@@ -39,11 +40,11 @@ export class ProductListComponent implements OnInit, OnDestroy {
         (products: Product[]) => (this.products = products),
         (err: any) => (this.errorMessage = err.error)
       );
-    this.store.pipe(select("products")).subscribe(products => {
-      if (products) {
-        this.displayCode = products.showProductCode;
-      }
-    });
+    this.store
+      .pipe(select(fromProduct.getShowProductCode))
+      .subscribe(showProductCode => {
+        this.displayCode = showProductCode;
+      });
   }
 
   ngOnDestroy(): void {
