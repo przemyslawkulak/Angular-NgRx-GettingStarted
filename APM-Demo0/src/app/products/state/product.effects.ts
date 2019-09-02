535acc165b2e7608d4b6d590e13a6be78809ae5a
Przemysław Kulak <przemyslaw.kulak86@gmail.com>
2019-09-02 15:17:56 +0200

    Create, Update, Delete Operations

--- APM-Demo0/src/app/products/state/product.effects.ts
+++ APM-Demo0/src/app/products/state/product.effects.ts
@@ -4,7 +4,8 @@ import { ProductService } from "../product.service";
 import * as productActions from "./product.actions";
 import { mergeMap, map, catchError } from "rxjs/operators";
 import { Product } from "../product";
-import { of } from "rxjs";
+import { of, Observable } from "rxjs";
+import { Action } from "@ngrx/store";
 
 @Injectable()
 export class ProductEffects {
@@ -19,6 +20,43 @@ export class ProductEffects {
       this.productService.getProducts().pipe(
         map((products: Product[]) => new productActions.LoadSuccess(products)),
         catchError(err => of(new productActions.LoadFail(err)))
+      )
+    )
+  );
+  @Effect()
+  updateProducts$: Observable<Action> = this.actions$.pipe(
+    ofType(productActions.ProductActionTypes.UpdateProduct),
+    map((action: productActions.UpdateProduct) => action.payload),
+    mergeMap((product: Product) =>
+      this.productService.updateProduct(product).pipe(
+        map(
+          updatedProduct =>
+            new productActions.UpdateProductSuccess(updatedProduct)
+        ),
+        catchError(err => of(new productActions.UpdateProductFail(err)))
+      )
+    )
+  );
+  @Effect()
+  createProduct$: Observable<Action> = this.actions$.pipe(
+    ofType(productActions.ProductActionTypes.CreateProduct),
+    map((action: productActions.CreateProduct) => action.payload),
+    mergeMap((product: Product) =>
+      this.productService.createProduct(product).pipe(
+        map(newProduct => new productActions.CreateProductSuccess(newProduct)),
+        catchError(err => of(new productActions.CreateProductFail(err)))
+      )
+    )
+  );
+
+  @Effect()
+  deleteProduct$: Observable<Action> = this.actions$.pipe(
+    ofType(productActions.ProductActionTypes.DeleteProduct),
+    map((action: productActions.DeleteProduct) => action.payload),
+    mergeMap((productId: number) =>
+      this.productService.deleteProduct(productId).pipe(
+        map(() => new productActions.DeleteProductSuccess(productId)),
+        catchError(err => of(new productActions.DeleteProductFail(err)))
       )
     )
   );
--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -9,13 +9,13 @@ export interface State extends fromRoot.State {
 
 export interface ProductState {
   showProductCode: boolean;
-  currentProduct: Product;
+  currentProductId: number | null;
   products: Product[];
   error: string;
 }
 const initialState: ProductState = {
   showProductCode: true,
-  currentProduct: null,
+  currentProductId: null,
   products: [],
   error: ""
 };
@@ -26,10 +26,28 @@ export const getShowProductCode = createSelector(
   getProductFeatureState,
   state => state.showProductCode
 );
-
+export const getCurrentProductID = createSelector(
+  getProductFeatureState,
+  state => state.currentProductId
+);
 export const getCurrentProduct = createSelector(
   getProductFeatureState,
-  state => state.currentProduct
+  getCurrentProductID,
+  (state, currentProductId) => {
+    if (currentProductId === 0) {
+      return {
+        id: 0,
+        productName: "",
+        productCode: "New",
+        description: "",
+        starRating: 0
+      };
+    } else {
+      return currentProductId
+        ? state.products.find(p => p.id === currentProductId)
+        : null;
+    }
+  }
 );
 
 export const getProducts = createSelector(
@@ -52,26 +70,23 @@ export function reducer(
         ...state,
         showProductCode: action.payload
       };
+
     case ProductActionTypes.SetCurrentProduct:
       return {
         ...state,
-        currentProduct: { ...action.payload }
+        currentProductId: action.payload.id
       };
+
     case ProductActionTypes.ClearCurrentProduct:
       return {
         ...state,
-        currentProduct: null
+        currentProductId: null
       };
+
     case ProductActionTypes.InitializeCurrentProduct:
       return {
         ...state,
-        currentProduct: {
-          id: 0,
-          productName: "",
-          productCode: "New",
-          description: "",
-          starRating: 0
-        }
+        currentProductId: 0
       };
     case ProductActionTypes.LoadSuccess:
       return {
@@ -86,6 +101,51 @@ export function reducer(
         error: action.payload
       };
     }
+    case ProductActionTypes.UpdateProductSuccess:
+      const updatedProducts = state.products.map(item =>
+        action.payload.id === item.id ? action.payload : item
+      );
+      return {
+        ...state,
+        products: updatedProducts,
+        currentProductId: action.payload.id,
+        error: ""
+      };
+    case ProductActionTypes.UpdateProductFail: {
+      return {
+        ...state,
+        error: action.payload
+      };
+    }
+    case ProductActionTypes.CreateProductSuccess:
+      return {
+        ...state,
+        products: [...state.products, action.payload],
+        currentProductId: action.payload.id,
+        error: ""
+      };
+
+    case ProductActionTypes.CreateProductFail:
+      return {
+        ...state,
+        error: action.payload
+      };
+
+    case ProductActionTypes.DeleteProductSuccess:
+      return {
+        ...state,
+        products: state.products.filter(
+          product => product.id !== action.payload
+        ),
+        currentProductId: null,
+        error: ""
+      };
+
+    case ProductActionTypes.DeleteProductFail:
+      return {
+        ...state,
+        error: action.payload
+      };
     default:
       return state;
   }
